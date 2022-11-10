09a3ef30e2a08eb0ddd8cd2b4d8cc06cf6491b3a
Matilde De Jesus Rosario De Oliveira E Costa <matde-je@c2r3s1.42lisboa.com>
2022-11-10 22:58:26 +0000

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: matde-je <matde-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 16:31:55 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/07 20:01:54 by matde-je         ###   ########.fr       */
+/*   Updated: 2022/11/10 22:55:53 by matde-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,5 +71,8 @@ t_list			*ft_lstlast(t_list *lst);
 int				ft_lstsize(t_list *lst);
 void			ft_lstadd_front(t_list **lst, t_list *new);
 void			ft_lstadd_back(t_list **lst, t_list *new);
+
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void*));
 
 #endif
